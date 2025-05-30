7cccc484c71d6d40a45839b46b45f42c09b1c3dc
neuecc <ils@neue.cc>
2025-05-30 17:29:02 +0900

    init

---
+++ tests/CompilerBrain.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using Xunit.v3;
+
+namespace CompilerBrain.Tests;
+
+public class UnitTest1(ITestContextAccessor testContextAccessor)
+{
+    [Fact]
+    public async Task Test1()
+    {
+        var memory = new SessionMemory();
+
+        var id = CSharpMcpServer.Initialize(memory);
+
+        // open self
+
+        var currentTest = testContextAccessor.Current;
+        var asmPath = currentTest.TestAssembly!.AssemblyPath;
+        // var path = Path.Combine(asmPath, "../../", "../../CompilerBrain.Tests.csproj");
+        var path = Path.Combine(asmPath, "../../../../../../sandbox/ConsoleApp/ConsoleApp.csproj");
+
+
+
+        var diagnostics = await CSharpMcpServer.OpenCsharpProject(memory, id, path);
+
+
+        var json = JsonSerializer.Serialize(diagnostics);
+    }
+}
